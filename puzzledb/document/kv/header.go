990726e1fc812e7354d5753243387ebfbaad1b36
Satoshi Konno <skonno@cybergarage.org>
2023-04-11 00:21:48 +0900

    Add document.kv package

---
+++ puzzledb/document/kv/header.go
@@ -0,0 +1,49 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kv
+
+type HeaderType byte
+
+// DocumentType represents a document type.
+type DocumentType byte
+
+// KeyHeader represents a header for all keys.
+type KeyHeader [2]byte
+
+// Version represents a version.
+type Version byte
+
+// IndexType represents an index type.
+type IndexType byte
+
+func (header KeyHeader) Type() HeaderType {
+	return HeaderType(header[0])
+}
+
+func (header KeyHeader) Version() Version {
+	return VertionFromHeaderByte(header[1])
+}
+
+func (header KeyHeader) DocumentType() DocumentType {
+	return DocumentType(TypeFromHeaderByte(header[1]))
+}
+
+func (header KeyHeader) IndexType() IndexType {
+	return IndexType(TypeFromHeaderByte(header[1]))
+}
+
+func (header KeyHeader) Bytes() []byte {
+	return header[:]
+}
---
+++ puzzledb/document/kv/header_test.go
@@ -0,0 +1,96 @@
+// Copyright (C) 2022 The PuzzleDB Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package kv
+
+import (
+	"testing"
+)
+
+func TestKeyHeader(t *testing.T) {
+	type expected struct {
+		tp  HeaderType
+		ver Version
+		doc DocumentType
+		idx IndexType
+	}
+	testKeyHeaders := []struct {
+		header   KeyHeader
+		expected expected
+	}{
+		{
+			header: DatabaseKeyHeader,
+			expected: expected{
+				tp:  DatabaseObject,
+				ver: V1,
+				doc: CBOR,
+				idx: IndexType(0),
+			},
+		},
+		{
+			header: SchemaKeyHeader,
+			expected: expected{
+				tp:  SchemaObject,
+				ver: V1,
+				doc: CBOR,
+				idx: IndexType(0),
+			},
+		},
+		{
+			header: DocumentKeyHeader,
+			expected: expected{
+				tp:  DocumentObject,
+				ver: V1,
+				doc: CBOR,
+				idx: IndexType(0),
+			},
+		},
+		{
+			header: PrimaryIndexHeader,
+			expected: expected{
+				tp:  IndexObject,
+				ver: V1,
+				doc: DocumentType(0),
+				idx: PrimaryIndex,
+			},
+		},
+		{
+			header: SecondaryIndexHeader,
+			expected: expected{
+				tp:  IndexObject,
+				ver: V1,
+				doc: DocumentType(0),
+				idx: SecondaryIndex,
+			},
+		},
+	}
+	for _, key := range testKeyHeaders {
+		if key.header.Type() != key.expected.tp {
+			t.Errorf("%v != %v", key.header.Type(), key.expected.tp)
+		}
+		if key.header.Version() != key.expected.ver {
+			t.Errorf("%v != %v", key.header.Version(), key.expected.ver)
+		}
+		if key.expected.doc != DocumentType(0) {
+			if key.header.DocumentType() != key.expected.doc {
+				t.Errorf("%v != %v", key.header.DocumentType(), key.expected.doc)
+			}
+		}
+		if key.expected.idx != IndexType(0) {
+			if key.header.IndexType() != key.expected.idx {
+				t.Errorf("%v != %v", key.header.IndexType(), key.expected.idx)
+			}
+		}
+	}
+}
