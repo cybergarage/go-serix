e0ce33b126f2e4e449a2dac51f533b7357d6735b
Satoshi Konno <skonno@lycorp.co.jp>
2025-11-19 19:59:49 +0900

    chore: update copyright notices to go-serix Authors

--- serixtest/puzzledbtest/plugins/coder/document/coder_helper.go
+++ serixtest/puzzledbtest/plugins/coder/document/coder_helper.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 The PuzzleDB Authors.
+// Copyright (C) 2022 The go-serix Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ import (
 	"testing"
 
 	"github.com/cybergarage/go-pict/pict"
-	"github.com/cybergarage/puzzledb-go/puzzledb/document"
+	"github.com/cybergarage/go-serix/serix/document"
 )
 
 //go:embed go_types.pict
