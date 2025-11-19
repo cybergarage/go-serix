174936b4f25dbb6580eb6545f53c086072e63293
Satoshi Konno <skonno@lycorp.co.jp>
2025-11-19 12:31:28 +0900

    chore: update copyright notices to go-serix Authors

--- puzzledb/document/index_impl.go
+++ puzzledb/document/index_impl.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 The PuzzleDB Authors.
+// Copyright (C) 2022 The go-serix Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
