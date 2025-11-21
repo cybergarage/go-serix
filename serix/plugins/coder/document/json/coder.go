de85f3c587ebede7010409b0b1369dda7b90bc45
Satoshi Konno <skonno@cybergarage.org>
2025-11-21 21:02:52 +0900

    feat: add JSON document coder

---
+++ serix/plugins/coder/document/json/coder.go
@@ -0,0 +1,58 @@
+// Copyright (C) 2022 The go-serix Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package json
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/cybergarage/go-serix/serix/document"
+)
+
+// Coder represents a JSON serializer.
+type Coder struct {
+}
+
+// NewCoder returns a new JSON serializer instance.
+func NewCoder() *Coder {
+	return &Coder{}
+}
+
+// EncodeDocument writes the specified object to the specified writer.
+func (s *Coder) EncodeDocument(w io.Writer, obj document.Object) error {
+	encoder := json.NewEncoder(w)
+	return encoder.Encode(obj)
+}
+
+// DecodeDocument returns the decoded object from the specified reader if available, otherwise returns an error.
+func (s *Coder) DecodeDocument(r io.Reader) (document.Object, error) {
+	var obj document.Object
+	decoder := json.NewDecoder(r)
+	err := decoder.Decode(&obj)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+// Start starts this coder.
+func (s *Coder) Start() error {
+	return nil
+}
+
+// Stop stops this coder.
+func (s *Coder) Stop() error {
+	return nil
+}
