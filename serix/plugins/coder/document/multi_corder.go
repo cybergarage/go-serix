eb9edaa992eeae0c0bdbb51fbb28efd4dc84d828
Satoshi Konno <skonno@cybergarage.org>
2025-12-04 20:05:10 +0900

    feat: add MultiCoder for layered encode/decode

---
+++ serix/plugins/coder/document/multi_corder.go
@@ -0,0 +1,69 @@
+// Copyright (C) 2022 The go-serix Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package document
+
+import (
+	"bytes"
+	"errors"
+	"io"
+
+	"github.com/cybergarage/go-serix/serix/document"
+)
+
+type multiCorder struct {
+	coders []document.Coder
+}
+
+func NewMultiCoder(coders ...document.Coder) document.Coder {
+	return &multiCorder{
+		coders: coders,
+	}
+}
+
+// EncodeDocument writes the specified object to the specified writer.
+func (m *multiCorder) EncodeDocument(w io.Writer, obj document.Object) error {
+	nextObject := obj
+	var lastWriter *bytes.Buffer
+	for _, coder := range m.coders {
+		if lastWriter == nil {
+			lastWriter = bytes.NewBuffer(nil)
+		} else {
+			lastWriter.Reset()
+		}
+		if err := coder.EncodeDocument(lastWriter, nextObject); err != nil {
+			return err
+		}
+		nextObject = lastWriter.Bytes()
+	}
+	if lastWriter == nil {
+		return nil
+	}
+	_, err := w.Write(lastWriter.Bytes())
+	return err
+}
+
+// DecodeDocument returns the decorded object from the specified reader if available, otherwise returns an error.
+func (m *multiCorder) DecodeDocument(r io.Reader) (document.Object, error) {
+	var lastErr error
+	for i := len(m.coders) - 1; i >= 0; i-- {
+		obj, err := m.coders[i].DecodeDocument(r)
+		if err != nil {
+			lastErr = errors.Join(lastErr, err)
+			continue
+		}
+		return obj, nil
+	}
+	return nil, lastErr
+}
